c3db34ce9c3d82a41923d1549a4da64056e98ccf
Csaba Olajos <olajos.csaba.88@gmail.com>
2019-10-20 14:17:13 +0200

    Rollback - oops comment fix

--- tests/Unit/MigraterRollbackTest.php
+++ tests/Unit/MigraterRollbackTest.php
@@ -6,54 +6,54 @@ namespace OlajosCs\Migrater\Tests\Unit;
 
 class MigraterRollbackTest extends MigraterTest
 {
-//    public function test_rollback_is_working(): void
-//    {
-//        $migration1 = new ExampleMigration1($this->pdo);
-//        $migration2 = new ExampleMigration2($this->pdo);
-//
-//        $this->migrater->register($migration1, $migration2);
-//        $this->migrater->migrateAll();
-//
-//        $result = $this->migrater->rollback();
-//        $this->assertEquals($result, $migration2->getKey());
-//
-//        $this->expectException(\PDOException::class);
-//        $this->pdo->query('select * from examples_2');
-//
-//    }
-//
-//    public function test_rollback_is_working_one(): void
-//    {
-//        $migration1 = new ExampleMigration1($this->pdo);
-//        $migration2 = new ExampleMigration2($this->pdo);
-//
-//        $this->migrater->register($migration1, $migration2);
-//        $this->migrater->migrateAll();
-//
-//        $result = $this->migrater->rollback();
-//        $this->assertEquals($result, $migration2->getKey());
-//
-//        $this->assertInstanceOf(\PDOStatement::class, $this->pdo->query('select * from examples_1'));
-//    }
-//
-//
-//    public function test_rollback_is_working_multiple(): void
-//    {
-//        $migration1 = new ExampleMigration1($this->pdo);
-//        $migration2 = new ExampleMigration2($this->pdo);
-//
-//        $this->migrater->register($migration1, $migration2);
-//        $this->migrater->migrateAll();
-//
-//        $result1 = $this->migrater->rollback();
-//        $this->assertEquals($result1, $migration2->getKey());
-//
-//        $result2 = $this->migrater->rollback();
-//        $this->assertEquals($result2, $migration1->getKey());
-//
-//        $this->expectException(\PDOException::class);
-//        $this->pdo->query('select * from examples_1');
-//    }
+    public function test_rollback_is_working(): void
+    {
+        $migration1 = new ExampleMigration1($this->pdo);
+        $migration2 = new ExampleMigration2($this->pdo);
+
+        $this->migrater->register($migration1, $migration2);
+        $this->migrater->migrateAll();
+
+        $result = $this->migrater->rollback();
+        $this->assertEquals($result, $migration2->getKey());
+
+        $this->expectException(\PDOException::class);
+        $this->pdo->query('select * from examples_2');
+
+    }
+
+    public function test_rollback_is_working_one(): void
+    {
+        $migration1 = new ExampleMigration1($this->pdo);
+        $migration2 = new ExampleMigration2($this->pdo);
+
+        $this->migrater->register($migration1, $migration2);
+        $this->migrater->migrateAll();
+
+        $result = $this->migrater->rollback();
+        $this->assertEquals($result, $migration2->getKey());
+
+        $this->assertInstanceOf(\PDOStatement::class, $this->pdo->query('select * from examples_1'));
+    }
+
+
+    public function test_rollback_is_working_multiple(): void
+    {
+        $migration1 = new ExampleMigration1($this->pdo);
+        $migration2 = new ExampleMigration2($this->pdo);
+
+        $this->migrater->register($migration1, $migration2);
+        $this->migrater->migrateAll();
+
+        $result1 = $this->migrater->rollback();
+        $this->assertEquals($result1, $migration2->getKey());
+
+        $result2 = $this->migrater->rollback();
+        $this->assertEquals($result2, $migration1->getKey());
+
+        $this->expectException(\PDOException::class);
+        $this->pdo->query('select * from examples_1');
+    }
 
 
     public function test_only_registered_migration_is_rollbacked(): void
